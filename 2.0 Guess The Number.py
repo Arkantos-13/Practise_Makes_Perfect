c93c25aaf1296aeecfdc732bb07b2c1e1789d612
GeoP <64365608+Arkantos-13@users.noreply.github.com>
2021-07-14 00:12:16 +0300

    Add files via upload

---
+++ 2.0 Guess The Number.py
@@ -0,0 +1,34 @@
+import random
+
+#I used the module below, in order to convert 1 to 1st, 2 to 2st etc
+import inflect
+
+num = int(input('Please type an integer number as an upper bound: '))
+
+
+def Computer_Choice(num):
+    low = 1
+    high = num
+    choices = ''
+    count = 1
+    while choices != 'c':
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low   # Or guess = high, it's the same
+
+        choices = input(f'Is {guess} too high (H), too low (L), or correct (C)?? ').lower()
+
+        if choices == 'h':
+            high = guess - 1
+            count += 1
+        elif choices == 'l':
+            low = guess + 1
+            count += 1
+
+    num_to_letters = inflect.engine()
+
+    print(f'The computer guessed your secret number {guess} correctly, in the {num_to_letters.ordinal(count)} guess')
+
+
+Computer_Choice(num)
